17934502e3f91e36c9f7f92cb3260104b5a7b700
selahudin <sulehibnumeka@gmail.com>
2022-12-12 06:25:11 -0800

    NEW

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,4 +2,23 @@
 /**
  * main - Entry point
  *
-  
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
