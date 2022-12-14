8bd88e1adc96390182bd480e9076aa1b97567d12
selahudin <sulehibnumeka@gmail.com>
2022-12-14 09:10:05 -0800

    NEW

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code for the ALX student
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_tables(98);
+	_putchar('\n');
+	print_times_tables(12);
+	_putchar('\n');
+	return (0);
+}
