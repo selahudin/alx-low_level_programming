f3ef9c8f2e8e1ad0b902dab1edd584fd1d012e32
selahudin <sulehibnumeka@gmail.com>
2022-12-19 11:32:34 -0800

    NEW

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies
+ * @dest: it is destiny
+ * @src: it is copy
+ *
+ * Return: this return copy
+ */
+
+void *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
