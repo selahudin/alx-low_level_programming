49aec46ba3e05fc3c672b8c589da113c2482a3bb
selahudin <sulehibnumeka@gmail.com>
2022-12-19 10:26:16 -0800

    NEW

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - it prints a reverse string
+ * @s: this is the input string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	for (i = i - 1; s[i] != '\0'; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
