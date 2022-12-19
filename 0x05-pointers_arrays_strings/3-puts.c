1fbc8b32bab49acdf36ead203479e23d05b6a5b9
selahudin <sulehibnumeka@gmail.com>
2022-12-19 10:07:23 -0800

    NEW

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts -  write a function
+ * @str: this is my string input
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
+
