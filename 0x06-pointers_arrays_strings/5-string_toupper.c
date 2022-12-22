245efd8920b73bd8ff32ae80a9821c0bf44100b4
selahudin <sulehibnumeka@gmail.com>
2022-12-22 11:45:13 -0800

    NEW

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - it changes lo to up
+ * @str: parameter
+ * Return: returns
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i++])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] - 32;
+	}
+
+	return (str);
+}
