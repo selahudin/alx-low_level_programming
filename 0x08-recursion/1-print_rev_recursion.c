5bcc42474ad2ef6df1d4349ac1e3597d9543bd3a
selahudin <sulehibnumeka@gmail.com>
2023-01-05 10:44:15 -0800

    NEW

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,14 +7,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
-	}
-	else if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
+	} else
+		putchar('0');
 }
