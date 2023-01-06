45706811d8f5953810e81f6959ea1ead4a66b3bc
selahudin <sulehibnumeka@gmail.com>
2023-01-06 12:29:57 -0800

    NEW

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * *_strspn -  a function that gets the length of a prefix substring.
- * @s: a sring that can be evaluated.
- * @accept: it contain list of character.
+ * *_strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match in s
  *
- * Return: Returns the number of bytes in the initial segment of s.
- * which consist only of bytes from accept.
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
  */
-
-unsigned int _strspn(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, f, flag;
-
 
 	f = 0;
 
@@ -32,7 +30,6 @@ unsigned int _strspn(char *s, char *accept);
 			return (f);
 		}
 	}
-
 
 	return (0);
 }
