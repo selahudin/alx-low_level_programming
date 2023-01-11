504242a1e98dbdd568605323c097884d978308a1
selahudin <sulehibnumeka@gmail.com>
2023-01-11 04:46:17 -0800

    NEW

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - it is a program that prints all arguments it receives.
+ * @argc: argument counter.
+ * @argv: argument vector.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int l = 0;
+
+	for (; l < argc; l++)
+	
+	printf("%s\n", argv[l]);
+	
+
+	return (0);
+}
